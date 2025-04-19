7272f157d5196e1d29812ebc8824fe780eafd0b2
Ian Parra <ianparra200@gmail.com>
2025-04-19 16:42:45 -0400

    feat: navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopHeaderComponent } from "./components/common/top-header/top-header.component";
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopHeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
