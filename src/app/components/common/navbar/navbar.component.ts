25fb4e739ffce03fdbaf28f0afbb5e93d18ebf12
Ian Parra <ianparra200@gmail.com>
2025-04-24 11:04:18 -0400

    feat: navbar responsive

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isOpen = false;
 
+  toggleNavbar() {
+    this.isOpen = !this.isOpen;
+  }
 }
