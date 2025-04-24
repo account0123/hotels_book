21cc2c167dd104bc36407783acaf6fd0cb6ddca7
Ian Parra <ianparra200@gmail.com>
2025-04-24 17:15:45 -0400

    style: lista de hoteles responsiva

--- src/app/components/pages/hotel-grid/hotel-grid.component.ts
+++ src/app/components/pages/hotel-grid/hotel-grid.component.ts
@@ -8,6 +8,10 @@ import { HotelBoxComponent } from '../../common/hotel-box/hotel-box.component';
   styleUrl: './hotel-grid.component.scss'
 })
 export class HotelGridComponent {
-  hoteles = new Array(30);
+  hoteles = [
+    {id: '1',name: 'Hotel California', address: 'Los Angeles 322, California', price: 200, flags: 31, rating: 4.8},
+    {id: '2',name: 'Hotel Transilvania', address: 'Los Muermos 111, Transilvania', price: 250, flags: 31, rating: 4.7},
+    {id: '3',name: 'Hotel ISBI', address: 'Marcoleta 144, Santiago', price: 150, flags: 31, rating: 4.7},
+  ]
   precios = [100, 200, 300, 400, 500];
 }
